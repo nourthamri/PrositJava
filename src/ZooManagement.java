faf72845e33a9f67c36ab3a0a8f8e5856d7855a3
nourthamri <thamri.nour@esprit.tn>
2024-09-16 20:01:02 +0100

    instruction1

---
+++ src/ZooManagement.java
@@ -0,0 +1,10 @@
+public class ZooManagement {
+
+
+    int nbrCages = 20;
+    String zooName = "myZoo";
+    public static void main(String[] args) {
+        //Instruction 1
+        ZooManagement zoo = new ZooManagement();
+        System.out.println(zoo.zooName + " comporte " + zoo.nbrCages);}
+}
